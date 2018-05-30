5bced518b5cfdd7330b5c1dee4433226d7feb967
Clark Newell <williamcnewell@gmail.com>
2018-05-30 11:40:51 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// const app = require('./app')
+
+app.listen(function () {
+    console.log('Listening on ' + port)
+})
+
+
